224d3d01dee8ede04f2000bdc95604456ee79297
Robin Glen <robinglen@gmail.com>
2021-04-29 19:13:31 +0100

    write files

--- dist/index.js
+++ dist/index.js
@@ -12259,8 +12259,8 @@ const writeMarkupFiles = async (config) => {
     console.log(bookmarks)
 
     console.log('Writing files')
-    writeFileSync('./chrome.html', chrome)
-    writeFileSync('./README.md', readme)
+    writeFileSync('./chrome.html', bookmarks.chrome)
+    writeFileSync('./README.md', bookmarks.readme)
 }
 
 module.exports = writeMarkupFiles;
